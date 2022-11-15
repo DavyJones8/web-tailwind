5cc7d03235b7773049e33224b196ae43e416e116
Nacho <ignaciov@enubes.com>
2022-11-15 18:41:58 +0100

    Hasta clase 29

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
     theme: {
         extend: {
+            screens: {
+                'xs' : {'max': '768px'}
+            },
             backgroundImage: {
                 'body-pattern': "url('../assets/img/pattern.png')",
             },
@@ -10,7 +13,28 @@ module.exports = {
                 'secundario': '#FF4C29',
                 'gris'      : '#D2D2D2',
                 'oscuro'    : '#082032',
+            },
+            animation: {
+                'spin-low'   : 'spin 2s linear infinite',
+                'from-bellow': 'fromBellow 500ms linear',
+                'from-right' : 'fromRight 300ms linear',
+            },
+            keyframes: {
+                fromBellow:{
+                    '0%'  : { transfrom: 'translateY(0%)'},
+                    '50%' : { transform: 'translateY(200%)'},
+                    '100%': { transfrom: 'translateY(0%)'}
+                },
+                fromRight:{
+                    '0%'  : { transfrom: 'translateX(200%)'},
+                    '50%' : { transform: 'translateX(100%)'},
+                    '100%': { transfrom: 'translateX(0%)'}
+                },
             }
         }
+    },
+    variants: {
+        animation: ['responsive', 'hover', 'group-hover'],
+        animate  : ['responsive', 'hover', 'group-hover']
     }
 }
