f7eecf564949dd4cf25bca92398e0abd0a8dea1a
Nacho <ignaciov@enubes.com>
2022-11-15 13:48:29 +0100

    Hasta clase 20

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+  // tailwind.config.js
+module.exports = {
+    theme: {
+        extend: {
+            backgroundImage: {
+                'body-pattern': "url('../assets/img/pattern.png')",
+            },
+            colors : {
+                'principal' : '#2C39aB',
+                'secundario': '#FF4C29',
+                'gris'      : '#D2D2D2',
+                'oscuro'    : '#082032',
+            }
+        }
+    }
+}
